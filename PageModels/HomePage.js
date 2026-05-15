64cd17bca0daf9117ef9c80fe956ff075a80f300
Karna1919 <saikumarkandrathi7@gmail.com>
2026-05-15 18:00:17 +0530

    Changed

--- PageModels/HomePage.js
+++ PageModels/HomePage.js
@@ -1,14 +1,14 @@
 exports.Homepage = class Homepage {
 
-    constructor(page) {
+    constructor(page,expect) {
         this.page = page;
+        this.expect = expect;
         this.logoImage =this.page.locator("//a[@href='https://www.salesforce.com/in/']");
 
     }
 
   async  homepageTest(){
 
-    await this.logoImage.isVisible();
     await this.logoImage.click();
 
     }
